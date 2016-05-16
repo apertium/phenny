2483432d9df78bdb75b46e2d733cbff132adb37a
Jonathan <jonathan.north.washington@gmail.com>
2016-05-16 03:58:45 +0100

    updates for less crashing of repo pollers and monitors
    Conflicts:
    	modules/git.py

--- modules/git.py
+++ modules/git.py
@@ -77,7 +77,6 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         '''Handles POST requests for all hooks.'''
 
         # read and decode data
-        print('payload received; headers: '+str(self.headers))
         length = int(self.headers['Content-Length'])
         indata = self.rfile.read(length)
         post_data = urllib.parse.parse_qs(indata.decode('utf-8'))
@@ -95,10 +94,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         if 'GitHub' in self.headers['User-Agent']:
             event = self.headers['X-Github-Event']
             user = data['sender']['login']
-            if 'repository' in data:
-                repo = data['repository']['name']
-            elif 'organization' in data:
-                repo = data['organization']['login']+' (org)'
+            repo = data['repository']['name']            
             if event == 'commit_comment':
                 commit = data['comment']['commit_id'][:7]
                 comment = data['comment']['body']
@@ -183,22 +179,10 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
                 team = data['team']['name']
                 msgs.append('repository {:} added to team {:} {:}' \
                             .format(name, team))
-            elif event == 'ping':
-                org = data['organization']
-                sender = data['sender']
-                msgs.append('ping from {:}, org: {:}'\
-                            .format(sender, org))
             else:
                 msgs.append('sorry, event {:} not supported yet.'.format(event))
-                msgs.append(str(data.keys()))
-            #print("DEBUG:msgs: "+str(msgs))
-<<<<<<< HEAD
-        elif 'Jenkins' in self.headers['User-Agent']:
-            msgs.append('Jenkins: {}'.format(data['message']))
-        # not github or Jenkins
-=======
+                msgs.append(data.keys())
         # not github
->>>>>>> 6d47ac1... updates for less crashing of repo pollers and monitors
         elif "commits" in data:
             for commit in data['commits']:
                 try:
@@ -239,14 +223,8 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
                         self.phenny.bot.msg(chan, msg)
 
         # send OK code and notify firespeaker
-        #print("sending 200 response")
         self.send_response(200)
-        #self.finish()
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        #self.phenny.bot.msg("firespeaker", "200 OK: ")
-        print("DONE!")
-	
+        # self.phenny.bot.msg("firespeaker", "200 OK")
 
     def getBBFiles(self, filelist):
         '''Sort filelist into added, modified, and removed files

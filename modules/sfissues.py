0b22706cea5b7e80e7aae94d2cee0afbf5e987e2
jatinluthra14 <jatinluthra14@gmail.com>
2016-12-30 23:33:05 +0530

    Fixed formatting issues

--- modules/sfissues.py
+++ modules/sfissues.py
@@ -1,13 +1,13 @@
 import feedparser
  
 # Function to fetch the rss feed and return the parsed RSS
-def parseRSS( rss_url ):
-    return feedparser.parse( rss_url ) 
+def parse_rss(rss_url):
+    return feedparser.parse(rss_url) 
     
 # Function grabs the rss feed headlines (titles) and returns them as a list
-def getHeadlines( rss_url ):
+def getHeadlines(rss_url):
     tickets = []
-    feed = parseRSS( rss_url )
+    feed = parse_rss(rss_url)
     for ticketitem in feed['items']:
         tickets.append(ticketitem['title'])
     return tickets
@@ -17,10 +17,10 @@ def bugs(phenny,input):
     messages = []
     rep_messages = []
     url = phenny.config.sf_issues_url 
-    messages.extend( getHeadlines( url ) )
+    messages.extend( getHeadlines(url))
     for items in messages:
         if items[0] != '#':
             rep_messages.append(items)
-    phenny.say(input.nick + ": "+ ', '.join(rep_messages))
+    phenny.say('{}: {}'.format(input.nick, ', '.join(rep_messages)))
 
 bugs.rule = ('$nick', 'bugs!')

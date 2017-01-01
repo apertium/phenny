a019052c51a7ffec8613ec1e055848476f345745
william1835 <wlee753159@gmail.com>
2017-01-01 22:07:05 +0000

    Update iso639.py
    Added ISO 639-3 code when ISO 639-1 code is supplied, and vice versa.

--- modules/iso639.py
+++ modules/iso639.py
@@ -44,8 +44,6 @@ def iso639(phenny, input):
         thisCode = random.choice(list(phenny.iso_data.keys()))
         #ISOcodes[random.randint(0,len(ISOcodes)-1)]
         #random.choice(ISOcodes)
-    if thisCode in phenny.iso_data:
-        response = template % (thisCode, phenny.iso_data[thisCode])
     else:
         if len(thisCode) > 3:      # so that we don't get e.g. 'a'
             for oneCode, oneLang in phenny.iso_data.items():
@@ -55,6 +53,17 @@ def iso639(phenny, input):
                     else:
                         response = template % (oneCode, oneLang)
                     #phenny.say("%s %s %s" % (oneCode, oneLang.lower(), thisCode.lower()))
+        elif thisCode in phenny.iso_data:
+            altCode = None
+            if len(thisCode) == 2 and thisCode in phenny.iso_conversion_data:
+                altCode = phenny.iso_conversion_data[thisCode]
+            elif len(thisCode) == 3:
+                for iso1, iso3 in phenny.iso_conversion_data.items():
+                    if thisCode == iso3:
+                        altCode = iso1
+                        break
+            response = template % (thisCode + (", " + altCode if altCode else ""), phenny.iso_data[thisCode])
+
     if response == "":
         response = "Sorry, %s not found" % thisCode
 

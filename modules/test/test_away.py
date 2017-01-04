b5ebe7f93641684e02bea607a05eb38dce81cddc
jatinluthra14 <jatinluthra14@gmail.com>
2017-01-04 23:02:27 +0530

    Cleaning

--- modules/test/test_away.py
+++ modules/test/test_away.py
@@ -7,6 +7,7 @@ class TestQueue(unittest.TestCase):
         self.phenny = MagicMock()
         self.input = MagicMock()
         self.input.count.return_value = 0 # Mock Attribute to provide the messages
+        self.input.nick = 'Testseen'
         
     def testWhereIs(self):
         self.input = "whereis Testwhereis"
@@ -14,21 +15,18 @@ class TestQueue(unittest.TestCase):
         self.phenny.reply.assert_called_once_with('Sorry, Testwhereis seems to be AWOL...')
 
     def testAway(self):
-        self.input.nick = 'Testseen'
         away.away(self.phenny, self.input)
         self.input = "whereis Testseen"
         away.whereis(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with("Testseen said: I'm away right now")
 
     def testBack(self):
-        self.input.nick = 'Testseen'
         away.back(self.phenny, self.input)
         self.input = "whereis Testseen"
         away.whereis(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with("Testseen said: I'm around at the minute")
 
     def testAwayBack(self):
-        self.input.nick = 'Testseen'
         away.away(self.phenny, self.input)
         away.back(self.phenny, self.input)
         self.input = "whereis Testseen"
@@ -36,7 +34,6 @@ class TestQueue(unittest.TestCase):
         self.phenny.reply.assert_called_once_with("Testseen said: I'm around at the minute")
 
     def testBackAway(self):
-        self.input.nick = 'Testseen'
         away.back(self.phenny, self.input)
         away.away(self.phenny, self.input)
         self.input = "whereis Testseen"

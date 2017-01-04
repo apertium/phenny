e537312efd6f40fa901a0a32ca152fda29340926
jatinluthra14 <jatinluthra14@gmail.com>
2017-01-04 20:20:26 +0530

    Added two new tests
    Renamed 8ball module and few more changes
    
    Clean test
    
    Clean Away Test
    
    Fixed a Line
    
    Clean Test Away

---
+++ modules/test/test_away.py
@@ -0,0 +1,29 @@
+import unittest
+from mock import MagicMock, Mock
+from modules import away
+
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        self.phenny = MagicMock()
+        self.input = MagicMock()
+        
+    def testWhereIs(self):
+        self.input = "whereis Testwhereis"
+        away.whereis(self.phenny, self.input)
+        self.phenny.reply.assert_called_once_with('Sorry, Testwhereis seems to be AWOL...')
+
+    def testAway(self):
+        self.input.nick = 'Testseen'
+        self.input.count.return_value = 0
+        away.away(self.phenny, self.input)
+        self.input = "whereis Testseen"
+        away.whereis(self.phenny, self.input)
+        self.phenny.reply.assert_called_once_with("Testseen said: I'm away right now")
+
+    def testBack(self):
+        self.input.nick = 'Testseen'
+        self.input.count.return_value = 0
+        away.back(self.phenny, self.input)
+        self.input = "whereis Testseen"
+        away.whereis(self.phenny, self.input)
+        self.phenny.reply.assert_called_once_with("Testseen said: I'm around at the minute")

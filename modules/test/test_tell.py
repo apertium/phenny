5897cb53953e1f915df4ee779a4bb07f14757823
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2017-12-23 21:17:05 +0100

    Create aliases and reminders

--- modules/test/test_tell.py
+++ modules/test/test_tell.py
@@ -3,6 +3,7 @@ Tests for phenny's tell.py
 """
 
 import unittest
+import datetime
 from mock import MagicMock, patch
 from modules import tell
 
@@ -14,9 +15,24 @@ class TestTell(unittest.TestCase):
         self.input = MagicMock()
         self.phenny.nick = 'phenny'
 
+    def create_alias(self, alias):
+        self.input.group = lambda x: ['', 'add', alias][x]
+        tell.alias(self.phenny, self.input)
+        tell.aliasPairMerge(self.phenny, self.input.nick, alias)
+
+    def create_reminder(self, teller):
+        timenow = datetime.datetime.utcnow().strftime('%d %b %Y %H:%MZ')
+        self.phenny.reminders[teller] = [(teller, 'do', timenow, 'something')]
+
     def test_messageAlert(self):
         self.input.sender = '#testsworth'
         self.input.nick = 'Testsworth'
+
+        aliases = ['tester', 'testing', 'testmaster']
+
+        for alias in aliases:
+            self.create_alias(alias)
+            self.create_reminder(alias)
 
         tell.messageAlert(self.phenny, self.input)
 

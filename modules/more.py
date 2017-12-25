ea4fd4a27a96f804c42ffd13cfa223776ecd6bd2
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2017-12-25 21:48:00 +0100

    Simplify more.py code

--- modules/more.py
+++ modules/more.py
@@ -9,16 +9,18 @@ def setup(self):
 
 def break_up_fn(string, max_length):
     parts = []
-    tmp = ''
+
     while len(string) > max_length:
         tmp = string[:max_length]
+
         if ' ' in tmp[-4:]:
             tmp = string[:max_length-4] # or else no space for ' ...'
+
         while not tmp[-1] == ' ':
             tmp = tmp[:-1]
+
         string = string[len(tmp):] # also skips space at the end of tmp
         parts.append(tmp.strip() + ' ...')
-        tmp = ''
 
     parts.append(string)
     return parts
@@ -35,8 +37,7 @@ def add_messages(target, phenny, msg, break_up=break_up_fn):
         for msg in msgs:
             phenny.msg(target, msg)
     else:
-        phenny.msg(target, msgs[0])
-        msgs = msgs[1:]
+        phenny.msg(target, msgs.pop(0))
         phenny.msg(target, 'you have ' + str(len(msgs)) + ' more message(s). Please type ".more" to view them.')
         phenny.messages[caseless_nick] = msgs
 

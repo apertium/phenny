05608994d8076937e4785c554a90ae2e89d2671c
sashka <44439409+litt1e@users.noreply.github.com>
2018-10-30 21:33:51 +0300

    Create test_sasl.py

---
+++ modules/test/test_sasl.py
@@ -0,0 +1,26 @@
+import unittest
+from modules import sasl
+from mock import MagicMock
+ 
+class TestSasl(unittest.TestCase):
+    def setUp(self):
+        self.phenny = MagicMock()
+        self.input = MagicMock()
+        self.phenny.nick = 'tester'
+ 
+    def test_irc_cap(self):
+        d = {'LS':  ['multi-prefix', 'sasl', 'test'],
+             'ACK': ['sasl', 'test'],
+             'test':['test1']}
+        for val in d:
+            for b in d[val]:
+                self.input.args[0] = b
+                self.input.args[3] = val
+                sasl.irc_cap(self.phenny, self.input)
+ 
+    def test_irc_authenticated(self):
+        d = {'Test':'TestPassword', None:None}
+        for val in d:
+            self.phenny.config.nick = val
+            self.phenny.config.password = d[val]
+            sasl.irc_authenticated(self.phenny, self.input)

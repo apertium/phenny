1ed7c4188b14b1c853e8a60090786dc8d7989777
amuritna <amuritna@gmail.com>
2018-11-17 01:08:31 +0700

    add issue.py - create a new github issue
    for https://codein.withgoogle.com/dashboard/task-instances/5046772335378432/

---
+++ modules/issue.py
@@ -0,0 +1,70 @@
+"""
+issue.py - create a new GitHub issue
+author: amuritna
+"""
+
+import requests
+import json
+
+oauth_token = 'PUT OAUTH TOKEN HERE'
+
+gh_uri = 'https://api.github.com'
+allowed_owners = ['apertium'] # makes checking for valid owner/repo combo faster
+default_desc = 'This issue was automatically made by begiak, Apertium\'s beloved IRC bot, by the order of someone on #apertium. A human is yet to update the description.'
+
+def issue(phenny, input):
+    """ .issue <repository> <title> - create a new GitHub issue """    
+ 
+    gh_check = requests.head(gh_uri)
+    if gh_check.status_code != 200:
+        return phenny.reply('Sorry, GitHub is down.')
+
+    # check input validity
+    try:
+        if not input.group(1):
+            return phenny.reply('For which repository do I make this issue?')
+
+        content = input.group(1).strip()
+       
+        ghpath = content.split()[0].split('/')
+        
+        # check whether likely in an owner/repository combo format
+        if len(ghpath) != 2:
+            return phenny.reply('Sorry, that doesn\'t look like a valid repository path.')
+
+        owner = ghpath[0]
+        repo = ghpath[1]
+
+        if owner not in allowed_owners:
+            return phenny.reply('Sorry, you don\'t have permission to create an issue there.')
+
+        title = " ".join(content.split()[1:]).strip()
+        if len(title) < 1:
+            return phenny.reply('What is the title of the issue?')
+
+    except:
+        return phenny.reply('For which repository do I make this issue?')
+
+    # build and post HTTP request
+    req_target = gh_uri + '/repos/' + owner + '/' + repo + '/issues'
+    req_params = {'access_token': oauth_token}
+    req_body = json.dumps({
+        "title": title,
+        "body": default_desc
+    })
+
+    req_issue = requests.post(req_target, params=req_params, data=req_body)
+
+    # return final feedback - successfully created issue? repo not found?
+    if req_issue.status_code == 404:
+        return phenny.reply('It looks like that repository doesn\'t exist...')
+
+    return phenny.reply('Issue created. You can add a description at ' + req_issue.json()["html_url"])
+
+
+issue.commands = ['issue']
+issue.priority = 'medium'
+
+if __name__ == "__main__":
+        print(__doc__.strip())
+

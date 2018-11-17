02dcbb0e91fe145d17d640f170499c68287d1d60
amuritna <amuritna@gmail.com>
2018-11-17 11:57:56 +0700

    small fixes, better error messages

--- modules/issue.py
+++ modules/issue.py
@@ -2,18 +2,22 @@
 issue.py - create a new GitHub issue
 author: amuritna
 """
-
+# TODO: add tests
+# TODO: use web module
 import requests
 import json
 
+# TODO: use phenny config
 oauth_token = 'PUT OAUTH TOKEN HERE'
 
 gh_uri = 'https://api.github.com'
 allowed_owners = ['apertium'] # makes checking for valid owner/repo combo faster
 default_desc = 'This issue was automatically made by begiak, Apertium\'s beloved IRC bot, by the order of someone on #apertium. A human is yet to update the description.'
 
+invalidmsg = 'Invalid .issue command. Usage: .issue <owner>/<repository> <title>'
+
 def issue(phenny, input):
-    """ .issue <repository> <title> - create a new GitHub issue """    
+    """ .issue <owner>/<repository> <title> - create a new GitHub issue """    
  
     gh_check = requests.head(gh_uri)
     if gh_check.status_code != 200:
@@ -21,29 +25,28 @@ def issue(phenny, input):
 
     # check input validity
     try:
-        if not input.group(1):
-            return phenny.reply('For which repository do I make this issue?')
+    	if not input.group(1):
+        	return phenny.reply(invalidmsg)
 
-        content = input.group(1).strip()
-       
-        ghpath = content.split()[0].split('/')
+    	content = input.group(1).strip()
+    	ghpath = content.split()[0].split('/')
         
-        # check whether likely in an owner/repository combo format
-        if len(ghpath) != 2:
-            return phenny.reply('Sorry, that doesn\'t look like a valid repository path.')
+    	# check whether likely in an owner/repository combo format
+    	if len(ghpath) != 2:
+        	return phenny.reply(invalidmsg)
 
-        owner = ghpath[0]
-        repo = ghpath[1]
+    	owner = ghpath[0]
+    	repo = ghpath[1]
 
-        if owner not in allowed_owners:
-            return phenny.reply('Sorry, you don\'t have permission to create an issue there.')
+    	if owner not in allowed_owners:
+        	return phenny.reply('Begiak cannot create an issue there.')
 
-        title = " ".join(content.split()[1:]).strip()
-        if len(title) < 1:
-            return phenny.reply('What is the title of the issue?')
+    	title = " ".join(content.split()[1:]).strip()
+    	if len(title) < 1:
+        	return phenny.reply(invalidmsg)
 
-    except:
-        return phenny.reply('For which repository do I make this issue?')
+    except SyntaxError:
+        return phenny.reply(invalidmsg)
 
     # build and post HTTP request
     req_target = gh_uri + '/repos/' + owner + '/' + repo + '/issues'
@@ -64,6 +67,7 @@ def issue(phenny, input):
 
 issue.commands = ['issue']
 issue.priority = 'medium'
+issue.example = '.issue apertium/phenny Commit messages with multiple authors showing the less relevant one'
 
 if __name__ == "__main__":
         print(__doc__.strip())

ccfa28d77af796a42c2d2b7ae6c01a9b45a39d32
amuritna <amuritna@gmail.com>
2018-11-24 00:32:52 +0700

    test for success

--- modules/test/test_issue.py
+++ modules/test/test_issue.py
@@ -3,9 +3,21 @@ from mock import MagicMock
 from modules import issue
 
 class TestIssue(unittest.TestCase):
+    
     def setUp(self):
         self.phenny = MagicMock()
         self.input = MagicMock()
+        
+    @patch('modules.issue.post')
+    def testSuccess(self, mock_post):
+        mock_response = MagicMock()
+        mock_response.json.return_value = {
+        'html_url': 'https://github.com/test/test'
+    }
+    mock_post.return_value = mock_response
+    self.input.group.return_value = 'test'
+    issue.issue(self.phenny, self.input)
+    self.phenny.reply.assert_called_with('Issue created. You can add a description at https://github.com/test/test')
              
     def testIllegal(self):
         test = ['.issue', 'octocat/Hello-World Create an illegal issue.']

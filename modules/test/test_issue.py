74b9b42d15fe8218ecae50ad9e1a4605c4a4d8be
amuritna <amuritna@gmail.com>
2018-11-27 06:06:31 +0700

    Update test_issue.py

--- modules/test/test_issue.py
+++ modules/test/test_issue.py
@@ -16,15 +16,18 @@ class TestIssue(unittest.TestCase):
         }
         mock_post.return_value = mock_response
         self.input.group = lambda x: ['.issue' 'test/test Create a test issue.'][x]
+        self.input.group.return_value = mock_response
         issue.issue(self.phenny, self.input)
         self.phenny.reply.assert_called_with('Issue created. You can add a description at https://github.com/test/test')
              
     def test_illegal(self):
         self.input.group = lambda x: = ['.issue', 'octocat/Hello-World Create an illegal issue.']
+        self.input.group.return_value = 'Begiak cannot create an issue there.'
         issue.issue(self.phenny, self.input)
         self.phenny.reply.assert_called_with('Begiak cannot create an issue there.')
         
     def test_invalid(self):
         self.input.group = lambda x: = ['.issue', 'boing boing boing someone is hungry']
+        self.input.group.return_value = 'Invalid .issue command. Usage: .issue <owner>/<repository> <title>'
         issue.issue(self.phenny, self.input)
         self.phenny.reply.assert_called_with('Invalid .issue command. Usage: .issue <owner>/<repository> <title>')

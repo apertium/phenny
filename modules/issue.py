9c9bb44ece38173250d564d84465855d1c91e033
amuritna <amuritna@gmail.com>
2018-11-27 05:06:04 +0700

    indentation

--- modules/issue.py
+++ modules/issue.py
@@ -12,60 +12,60 @@ default_desc = 'This issue was automatically made by begiak, Apertium\'s beloved
 invalidmsg = 'Invalid .issue command. Usage: .issue <owner>/<repository> <title>'
 
 def issue(phenny, input):
-	""" .issue <owner>/<repository> <title> - create a new GitHub issue """    
+    """ .issue <owner>/<repository> <title> - create a new GitHub issue """    
  
-	if not is_up(gh_uri):
-		return phenny.reply('Sorry, GitHub is down.')
+    if not is_up(gh_uri):
+        return phenny.reply('Sorry, GitHub is down.')
 	
-	# check if GitHub auth token is available in default.py
-	try:
-		oauth_token = phenny.config.gh_oauth_token
-	except AttributeError:
-		return phenny.reply('GitHub authentication token needs to first be set in the configuration file (default.py)')
+    # check if GitHub auth token is available in default.py
+    try:
+        oauth_token = phenny.config.gh_oauth_token
+    except AttributeError:
+        return phenny.reply('GitHub authentication token needs to first be set in the configuration file (default.py)')
 		
-	# check input validity
-	try:
-		if not input.group(1):
-			return phenny.reply(invalidmsg)
+    # check input validity
+    try:
+        if not input.group(1):
+            return phenny.reply(invalidmsg)
 
-		content = input.group(1).strip()
-		ghpath = content.split()[0].split('/')
+        content = input.group(1).strip()
+        ghpath = content.split()[0].split('/')
         
-		# check whether likely in an owner/repository combo format
-		if len(ghpath) != 2:
-			return phenny.reply(invalidmsg)
+        # check whether likely in an owner/repository combo format
+        if len(ghpath) != 2:
+            return phenny.reply(invalidmsg)
 
-		owner = ghpath[0]
-		repo = ghpath[1]
+        owner = ghpath[0]
+        repo = ghpath[1]
 
-		if owner not in allowed_owners:
-			return phenny.reply('Begiak cannot create an issue there.')
+        if owner not in allowed_owners:
+            return phenny.reply('Begiak cannot create an issue there.')
 
-		title = " ".join(content.split()[1:]).strip()
-		if len(title) < 1:
-			return phenny.reply(invalidmsg)
+        title = " ".join(content.split()[1:]).strip()
+        if len(title) < 1:
+            return phenny.reply(invalidmsg)
 		
-	except SyntaxError:
-		return phenny.reply(invalidmsg)
+    except SyntaxError:
+        return phenny.reply(invalidmsg)
 
-	# build and post HTTP request
-	req_url = '%u/repos/%o/%r/issues' % (gh_uri, owner, repo)
-	req_headers = {'Authorization': 'token ' + oauth_token}
-	req_body = json.dumps({
-		"title": title,
-		"body": default_desc
-	})
+    # build and post HTTP request
+    req_url = '%u/repos/%o/%r/issues' % (gh_uri, owner, repo)
+    req_headers = {'Authorization': 'token ' + oauth_token}
+    req_body = json.dumps({
+        "title": title,
+        "body": default_desc
+    })
 
-	try:
-		req_str = post(req_url, req_body, req_headers)
-		req_json = json.loads(req_str)
-		return phenny.reply('Issue created. You can add a description at ' + req_json['html_url'])
-	except HTTPError:
-		return phenny.reply('It appears that that repository does not exist.')
+    try:
+        req_str = post(req_url, req_body, req_headers)
+        req_json = json.loads(req_str)
+        return phenny.reply('Issue created. You can add a description at ' + req_json['html_url'])
+    except HTTPError:
+        return phenny.reply('It appears that that repository does not exist.')
 
 issue.commands = ['issue']
 issue.priority = 'medium'
 issue.example = '.issue apertium/phenny Commit messages with multiple authors showing the less relevant one'
 
 if __name__ == "__main__":
-	print(__doc__.strip())
+    print(__doc__.strip())

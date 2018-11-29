2f8ae1ec46c5837f5532e931185ef3c4e593fc5f
amuritna <amuritna@gmail.com>
2018-11-29 20:46:33 +0700

    assert post request body, header

--- modules/test/test_issue.py
+++ modules/test/test_issue.py
@@ -7,6 +7,7 @@ class TestIssue(unittest.TestCase):
     def setUp(self):
         self.phenny = MagicMock()
         self.input = MagicMock()
+        self.phenny.nick = 'phenny'
         
     @patch('modules.issue.post')
     def test_success(self, mock_post):
@@ -15,9 +16,13 @@ class TestIssue(unittest.TestCase):
             'html_url': 'https://github.com/test/test'
         }
         mock_post.return_value = mock_response
-        mock_post.assert_called_with('https://api.github.com/repos/test/test/issues')
-        self.input.group = lambda x: ['.issue' 'test/test Create a test issue.'][x]
         self.input.group.return_value = mock_response
+        
+        mock_body = json.dumps({ "title": "Create a test issue.", "body": "This issue was automatically made by begiak, Apertium\'s beloved IRC bot, by the order of phenny on #apertium. A human is yet to update the description."})
+        mock_head = {'Authorization': 'token {}'.format(phenny.config.gh_oauth_token)}
+        mock_post.assert_called_with('https://api.github.com/repos/test/test/issues', mock_body, mock_head)
+        self.input.group = lambda x: ['.issue' 'test/test Create a test issue.'][x]
+        
         issue.issue(self.phenny, self.input)
         self.phenny.reply.assert_called_with('Issue created. You can add a description at https://github.com/test/test')
              

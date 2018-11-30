ba4c7398db6a22407626ff4cb9f943f488ccccf0
amuritna <amuritna@gmail.com>
2018-11-30 15:22:19 +0700

    That was a bad idea
    This reverts commit cfad9a3a33fcc754eaf81274e037770a8340ae28.

--- modules/test/test_issue.py
+++ modules/test/test_issue.py
@@ -26,10 +26,10 @@ class TestIssue(unittest.TestCase):
         
         mock_body = json.dumps({ "title": "Create a test issue.", "body": "This issue was automatically made by begiak, Apertium\'s beloved IRC bot, by the order of phenny on #apertium. A human is yet to update the description."})
         mock_head = {'Authorization': 'token test_token'}
+        mock_post.assert_called_with('https://api.github.com/repos/test/test/issues', mock_body, mock_head)
         
         self.input.group = lambda x: ['.issue' 'test/test Create a test issue.'][x]  
         issue.issue(self.phenny, self.input)
-        mock_post.assert_called_with('https://api.github.com/repos/test/test/issues', mock_body, mock_head)
         self.phenny.reply.assert_called_with('Issue created. You can add a description at https://github.com/test/test')
              
     def test_illegal(self):

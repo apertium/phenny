66e7dc7d1f4368cf95f2aae3152b8ed92249240b
Avyay Varadarajan <avyayv@gmail.com>
2018-12-02 16:55:48 -0800

    fixed

--- apertium_wikistats_bot.py
+++ apertium_wikistats_bot.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
-import argparse, requests, json, logging, sys, re, os, subprocess, shutil, importlib, urllib.request, collections, tempfile
-
+import argparse
+import requests
+import json
+import logging
+import sys
+import re
+import os
+import subprocess
+import shutil
+import importlib
+import urllib.request
+import collections
+import tempfile
 import xml.etree.ElementTree as etree
 import datetime
 import autocoverage
+
 wikiURL = 'http://wiki.apertium.org/wiki/'
 apiURL = 'http://wiki.apertium.org/w/api.php'
 statsURL = 'http://apertium.projectjj.com/stats-service/apertium-%s/?async=false'
@@ -12,40 +24,26 @@ githubBlobUrl = 'https://raw.githubusercontent.com/apertium/%s/master/%s'
 
 s = requests.Session()
 
-def queryForValue(rawStats, fileKind, stat_kind):
-    if stat_kind:
-        for stat in rawStats:
-            if stat['file_kind'] == fileKind and stat['stat_kind'] == stat_kind:
-                return stat['value']
+countDict = {
+'Monodix': {'monodix stems': 'Stems', 'paradigms': 'Paradigms'},
+'MetaMonodix': {'meta stems': 'Stems', 'meta paradigms': 'Paradigms'},
+'Bidix': {'stems': 'Stems'},
+'MetaBidix': {'meta stems': 'Stems'},
+'Lexc': {'lexc stems': 'Stems', 'vanilla stems': 'VanillaStems'},
+'Rlx': {'rlx rules': 'Rules'}
+}
+
+def queryForValue(rawStats, fileKind, statKind):
+    for stat in rawStats:
+        if stat['file_kind'] == fileKind and stat['stat_kind'] == statKind:
+            return stat['value']
 
 def getCounts(rawStats, fileFormat):
-    if fileFormat == 'Monodix':
-        return {
-            'stems': queryForValue(rawStats, fileFormat, 'Stems'),
-            'paradigms': queryForValue(rawStats, fileFormat, 'Paradigms')
-        }
-    elif fileFormat == 'MetaMonodix':
-        return {
-            'meta stems': queryForValue(rawStats, fileFormat, 'Stems'),
-            'meta paradigms': queryForValue(rawStats, fileFormat, 'Paradigms')
-        }
-    elif fileFormat == 'Bidix':
-        return {
-            'stems': queryForValue(rawStats, fileFormat, 'Stems')
-        }
-    elif fileFormat == 'MetaBidix':
-        return {
-            'meta stems': queryForValue(rawStats, fileFormat, 'Stems')
-        }
-    elif fileFormat == 'Lexc':
-        return {
-            'stems': queryForValue(rawStats, fileFormat, 'Stems'),
-            'vanilla stems': queryForValue(rawStats, fileFormat, 'VanillaStems')
-        }
-    elif fileFormat == 'Rlx':
-        return {
-            'rlx rules': queryForValue(rawStats, fileFormat, 'Rules')
-        }
+    count = {}
+    if fileFormat in countDict:
+        for key in countDict[fileFormat]:
+            count[key] = queryForValue(rawStats, fileFormat, countDict[fileFormat][key])
+        return count
 
 def countAllStats(rawStats, arr):
     fileCounts = {}
@@ -55,8 +53,6 @@ def countAllStats(rawStats, arr):
         pair = stat['name']
         lang_pair_to_post = pair.split('apertium-')[1]
         counts = getCounts(rawStats, format)
-        if format == 'MetaMonodix':
-            lang_pair_to_post = toISO(fileLoc.split('.')[1].split('.')[0])
         if counts:
             for countType, count in counts.items():
                 revisionInfo = getRevisionInfo(rawStats, format)
@@ -75,30 +71,28 @@ def getJSONFromStatsService(lang):
             isoCodeLangPair = toAlpha3Code(indLangs[0]) + '-'
         if toAlpha3Code(indLangs[1]):
             isoCodeLangPair += toAlpha3Code(indLangs[1])
-        if isoCodeLangPair != "":
-            pairForUrl = isoCodeLangPair
-        url = statsURL % pairForUrl
-        rawStats = s.post(url).json()
-    else:
-        if isoCodeLangPair != "":
-            pairForUrl = isoCodeLangPair
-        url = statsURL % pairForUrl
-        rawStats = s.post(url).json()
+    if isoCodeLangPair != "":
+        pairForUrl = isoCodeLangPair
+    url = statsURL % pairForUrl
+    rawStats = s.post(url).json()
     if 'stats' in rawStats:
         return rawStats['stats']
     logging.error('Unable to find %s' % isoCodeLangPair)
 
 def monoLangInformation(typeOfDict, rawStats, fileCounts):
-    for stat in rawStats:
-        if stat['file_kind'] == 'Monodix':
-            fileKind = stat['file_kind']
-            fileLoc = stat['path']
-            fileName = stat['name']
-    counts = getCounts(rawStats, typeOfDict)
-    for countType, count in counts.items():
-        revisionInfo = getRevisionInfo(rawStats, fileKind)
-        if revisionInfo:
-            fileCounts[countType] = (count, revisionInfo, githubBlobUrl % (fileName, fileLoc))
+    try:
+        for stat in rawStats:
+            if stat['file_kind'] == typeOfDict:
+                fileKind = stat['file_kind']
+                fileLoc = stat['path']
+                fileName = stat['name']
+        counts = getCounts(rawStats, typeOfDict)
+        for countType, count in counts.items():
+            revisionInfo = getRevisionInfo(rawStats, fileKind)
+            if revisionInfo:
+                fileCounts[countType] = (count, revisionInfo, githubBlobUrl % (fileName, fileLoc))
+    except UnboundLocalError:
+        pass
 
 def getMonoLangCounts(rawStats):
     fileCounts = {}
@@ -227,7 +221,8 @@ def addCategory(pageContents):
 
 def toISO(code):
     iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
-    return iso639Codes[code]
+    if code in iso639Codes:
+        return iso639Codes[code]
 
 def toAlpha3Code(code):
     iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
@@ -392,6 +387,10 @@ if __name__ == '__main__':
             try:
                 langs = pair.split('-')
                 pageTitle = 'Apertium-' + '-'.join(langs) + '/stats'
+                if len(langs) == 2 and (not(getPage(pageTitle))):
+                    if toISO(langs[0]) and toISO(langs[1]):
+                        langs[0] = toISO(langs[0])
+                        langs[1] = toISO(langs[1])
                 if len(langs) == 1:
                     pageTitle = 'Apertium-' + toAlpha3Code(langs[0]) + '/stats'
             except:

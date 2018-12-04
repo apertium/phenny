0bf5277500350f751b4b2a2fde0e735607f19373
Avyay Varadarajan <avyayv@gmail.com>
2018-12-03 21:54:03 -0800

    fixes

--- apertium_wikistats_bot.py
+++ apertium_wikistats_bot.py
@@ -24,14 +24,15 @@ githubBlobUrl = 'https://raw.githubusercontent.com/apertium/%s/master/%s'
 
 s = requests.Session()
 
-fileStatTypeMapping = {'Monodix': {'stems': 'Stems', 'paradigms': 'Paradigms'},
+fileStatTypeMapping = {
+    'Monodix': {'stems': 'Stems', 'paradigms': 'Paradigms'},
     'MetaMonodix': {'meta stems': 'Stems', 'meta paradigms': 'Paradigms'},
     'Bidix': {'stems': 'Entries'},
     'MetaBidix': {'meta stems': 'Stems'},
     'Lexc': {'stems': 'Stems', 'vanilla stems': 'VanillaStems'},
     'Rlx': {'rlx rules': 'Rules'},
     'Transfer': {'rules': 'Rules', 'macros': 'Macros'}
-    }
+}
 
 def getStatValue(rawStats, fileKind, statKind, pair=None, fileLoc=None):
     for stat in rawStats:
@@ -39,9 +40,12 @@ def getStatValue(rawStats, fileKind, statKind, pair=None, fileLoc=None):
             langAndExtension = stat['path'].split('.')
             pairInFile = langAndExtension[1]
             extension = langAndExtension[-1]
-            extensionMatches = (extension == fileLoc.split('.')[-1])
-            pairMatches = (pair == pairInFile)
-            if stat['file_kind'] == fileKind and stat['stat_kind'] == statKind and pairMatches and extensionMatches:
+            extensionMatches = extension == fileLoc.split('.')[-1]
+            pairMatches = pair == pairInFile
+            if stat['file_kind'] == fileKind and \
+               stat['stat_kind'] == statKind and \
+               pairMatches and \
+               extensionMatches:
                 return stat['value']
         else:
             if stat['file_kind'] == fileKind and stat['stat_kind'] == statKind:
@@ -51,16 +55,20 @@ def getCounts(rawStats, fileFormat, langPairToPost=None, fileLoc=None):
     count = {}
     if fileFormat in fileStatTypeMapping:
         for key in fileStatTypeMapping[fileFormat]:
-            if getStatValue(rawStats, fileFormat, fileStatTypeMapping[fileFormat][key]) != None:
+            if getStatValue(rawStats, fileFormat, fileStatTypeMapping[fileFormat][key]) is not None:
                 wikiKey = key
                 if langPairToPost and fileLoc:
                     for stat in rawStats:
                         langAndExtension = stat['path'].split('.')
                         pairInFile = langAndExtension[1]
                         extension = langAndExtension[-1]
-                        extensionAndPairEqual = (langPairToPost == pairInFile and extension == fileLoc.split('.')[-1])
+                        pairEqual = langPairToPost == pairInFile
+                        extensionEqual = extension == fileLoc.split('.')[-1]
                         statKindsEqual = (stat['stat_kind'] == fileStatTypeMapping[fileFormat][key])
-                        if stat['file_kind'] == 'Transfer' and statKindsEqual and extensionAndPairEqual:
+                        if stat['file_kind'] == 'Transfer' and \
+                           statKindsEqual and \
+                           extensionEqual and \
+                           pairEqual:
                             wikiKey = extension + ' ' + key
                             count[langPairToPost + ' ' + wikiKey] = getStatValue(rawStats, fileFormat, fileStatTypeMapping[fileFormat][key], langPairToPost, fileLoc)
                     count[langPairToPost + ' ' + wikiKey] = getStatValue(rawStats, fileFormat, fileStatTypeMapping[fileFormat][key], langPairToPost, fileLoc)
@@ -72,7 +80,7 @@ def countAllStats(rawStats, arr):
     fileCounts = {}
     if rawStats:
         for stat in rawStats:
-            fileFormat = stat['file_kind']
+            format = stat['file_kind']
             fileLoc = stat['path']
             pair = stat['name']
             lastAuthor = stat['last_author']
@@ -87,30 +95,26 @@ def countAllStats(rawStats, arr):
     return fileCounts
 
 def getJSONFromStatsService(lang):
-    try:
-        pairForUrl = lang
-        isoCodeLangPair = None
-        indLangs = pairForUrl.split('-')
-        isoCodeLangPair = '-'.join(list(map(toAlpha3Code, indLangs)))
-        if isoCodeLangPair:
-            pairForUrl = isoCodeLangPair
-        url = statsURL % pairForUrl
-        rawStats = s.post(url).json()
-        if 'stats' in rawStats:
-            return rawStats['stats']
-        raise ValueError('Cannot find this module')
-    except ValueError:
-        logging.error('Unable to request stats for %s' % isoCodeLangPair)
+    pairForUrl = lang
+    isoCodeLangPair = None
+    indLangs = pairForUrl.split('-')
+    isoCodeLangPair = '-'.join(list(map(toAlpha3Code, indLangs)))
+    pairForUrl = isoCodeLangPair
+    url = statsURL % pairForUrl
+    rawStats = s.post(url).json()
+    if 'stats' in rawStats:
+        return rawStats['stats']
+    logging.error('Unable to request stats for %s' % isoCodeLangPair)
 
-def monoLangInformation(fileFormat, rawStats, fileCounts):
+def monoLangInformation(typeOfDict, rawStats, fileCounts):
     for stat in rawStats:
-        if stat['file_kind'] == fileFormat:
+        if stat['file_kind'] == typeOfDict:
             fileKind = stat['file_kind']
             fileLoc = stat['path']
             fileName = stat['name']
             revisionNumber = stat['revision']
             revisionAuthor = stat['last_author']
-    counts = getCounts(rawStats, fileFormat)
+    counts = getCounts(rawStats, typeOfDict)
     for countType, count in counts.items():
         revisionInfo = (revisionNumber, revisionAuthor)
         if revisionInfo:
@@ -242,6 +246,8 @@ def toISO(code):
     iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
     if code in iso639Codes:
         return iso639Codes[code]
+    else:
+        return code
 
 def toAlpha3Code(code):
     iso639Codes = {"abk":"ab","aar":"aa","afr":"af","aka":"ak","sqi":"sq","amh":"am","ara":"ar","arg":"an","hye":"hy","asm":"as","ava":"av","ave":"ae","aym":"ay","aze":"az","bam":"bm","bak":"ba","eus":"eu","bel":"be","ben":"bn","bih":"bh","bis":"bi","bos":"bs","bre":"br","bul":"bg","mya":"my","cat":"ca","cha":"ch","che":"ce","nya":"ny","zho":"zh","chv":"cv","cor":"kw","cos":"co","cre":"cr","hrv":"hr","ces":"cs","dan":"da","div":"dv","nld":"nl","dzo":"dz","eng":"en","epo":"eo","est":"et","ewe":"ee","fao":"fo","fij":"fj","fin":"fi","fra":"fr","ful":"ff","glg":"gl","kat":"ka","deu":"de","ell":"el","grn":"gn","guj":"gu","hat":"ht","hau":"ha","heb":"he","her":"hz","hin":"hi","hmo":"ho","hun":"hu","ina":"ia","ind":"id","ile":"ie","gle":"ga","ibo":"ig","ipk":"ik","ido":"io","isl":"is","ita":"it","iku":"iu","jpn":"ja","jav":"jv","kal":"kl","kan":"kn","kau":"kr","kas":"ks","kaz":"kk","khm":"km","kik":"ki","kin":"rw","kir":"ky","kom":"kv","kon":"kg","kor":"ko","kur":"ku","kua":"kj","lat":"la","ltz":"lb","lug":"lg","lim":"li","lin":"ln","lao":"lo","lit":"lt","lub":"lu","lav":"lv","glv":"gv","mkd":"mk","mlg":"mg","msa":"ms","mal":"ml","mlt":"mt","mri":"mi","mar":"mr","mah":"mh","mon":"mn","nau":"na","nav":"nv","nob":"nb","nde":"nd","nep":"ne","ndo":"ng","nno":"nn","nor":"no","iii":"ii","nbl":"nr","oci":"oc","oji":"oj","chu":"cu","orm":"om","ori":"or","oss":"os","pan":"pa","pli":"pi","fas":"fa","pol":"pl","pus":"ps","por":"pt","que":"qu","roh":"rm","run":"rn","ron":"ro","rus":"ru","san":"sa","srd":"sc","snd":"sd","sme":"se","smo":"sm","sag":"sg","srp":"sr","gla":"gd","sna":"sn","sin":"si","slk":"sk","slv":"sl","som":"so","sot":"st","azb":"az","spa":"es","sun":"su","swa":"sw","ssw":"ss","swe":"sv","tam":"ta","tel":"te","tgk":"tg","tha":"th","tir":"ti","bod":"bo","tuk":"tk","tgl":"tl","tsn":"tn","ton":"to","tur":"tr","tso":"ts","tat":"tt","twi":"tw","tah":"ty","uig":"ug","ukr":"uk","urd":"ur","uzb":"uz","ven":"ve","vie":"vi","vol":"vo","wln":"wa","cym":"cy","wol":"wo","fry":"fy","xho":"xh","yid":"yi","yor":"yo","zha":"za","zul":"zu", "hbs":"sh", "arg":"an", "pes":"fa"}
@@ -406,10 +412,8 @@ if __name__ == '__main__':
             try:
                 langs = pair.split('-')
                 pageTitle = 'Apertium-' + '-'.join(langs) + '/stats'
-                if len(langs) == 2 and (not(getPage(pageTitle))):
-                    if toISO(langs[0]) and toISO(langs[1]):
-                        langs[0] = toISO(langs[0])
-                        langs[1] = toISO(langs[1])
+                if len(langs) == 2 and not(getPage(pageTitle)):
+                    langs = list(map(toISO, langs))
                 if len(langs) == 1:
                     pageTitle = 'Apertium-' + toAlpha3Code(langs[0]) + '/stats'
             except:

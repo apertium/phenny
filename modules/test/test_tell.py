54fbd2e8e3d11b05f56b61760f071158f1d87535
Maathavan <maathumaathavan@gmail.com>
2020-01-20 12:14:37 +0530

    Revert "Update test_tell.py"
    This reverts commit aaf32d8b7fb8d041d0f71cc3f61d5aa4aed57128.

--- modules/test/test_tell.py
+++ modules/test/test_tell.py
@@ -17,46 +17,14 @@ class TestTell(unittest.TestCase):
         self.input = MagicMock()
         tell.setup(self.phenny)
 
-    def create_reminder(self, teller):
-        timenow = datetime.datetime.utcnow().strftime('%d %b %Y %H:%MZ')
-        self.phenny.reminders[teller] = [(teller, 'do', timenow, 'something')]
-
-    def test_fremind_toolong(self):
-        self.input.nick = 'Testsworth'
-        self.input.groups = lambda: ['testerrrrrrrrrrrrrrrrr', 'eat a cake']
-
-        tell.f_remind(self.phenny, self.input, 'ask')
-        self.phenny.reply.assert_called_once_with('That nickname is too long.')
-
-    def test_fremind_tellself(self):
-        self.input.nick = 'Testsworth'
-        self.create_alias('tests', self.input)
-
-        self.input.groups = lambda: ['tests', 'eat a cake']
-
-        tell.f_remind(self.phenny, self.input, 'ask')
-        self.phenny.say.assert_called_once_with('You can ask yourself that.')
-
-    def test_fremind_edgecase(self):
-        self.input.nick = 'Testsworth'
-        self.input.groups = lambda: ['me', 'eat a cake']
-
-        tell.f_remind(self.phenny, self.input, 'ask')
-        self.phenny.say.assert_called_once_with('Hey, I\'m not as stupid as Monty you know!')
-
-    def test_formatreminder(self):
-        dt = datetime.datetime.utcnow().strftime('%d %b %Y %H:%MZ')
-        ret = tell.formatReminder(['tests', 'ask', dt, 'to eat cake'], 'Testsworth', None)
-
-        dt = dt[len(datetime.datetime.utcnow().strftime('%d %b')) + 1:]
-        dt = dt.replace(datetime.datetime.utcnow().strftime('%Y '), '')
-        self.assertTrue(ret == 'Testsworth: %s <tests> ask Testsworth to eat cake' % dt)
-
-"""
     def create_alias(self, aliasName, input):
         self.input.group = lambda x: ['', 'add', aliasName][x]
         alias.alias(self.phenny, input)
         alias.aliasPairMerge(self.phenny, input.nick, aliasName)
+
+    def create_reminder(self, teller):
+        timenow = datetime.datetime.utcnow().strftime('%d %b %Y %H:%MZ')
+        self.phenny.reminders[teller] = [(teller, 'do', timenow, 'something')]
 
     def test_messageAlert(self):
         self.input.sender = '#testsworth'
@@ -74,6 +42,22 @@ class TestTell(unittest.TestCase):
         text = ': You have messages. Say something, and I\'ll read them out.'
         self.phenny.say.assert_called_once_with(self.input.nick + text)
 
+    def test_fremind_toolong(self):
+        self.input.nick = 'Testsworth'
+        self.input.groups = lambda: ['testerrrrrrrrrrrrrrrrr', 'eat a cake']
+
+        tell.f_remind(self.phenny, self.input, 'ask')
+        self.phenny.reply.assert_called_once_with('That nickname is too long.')
+
+    def test_fremind_tellself(self):
+        self.input.nick = 'Testsworth'
+        self.create_alias('tests', self.input)
+
+        self.input.groups = lambda: ['tests', 'eat a cake']
+
+        tell.f_remind(self.phenny, self.input, 'ask')
+        self.phenny.say.assert_called_once_with('You can ask yourself that.')
+
     def test_fremind_valid(self):
         self.input.nick = 'Testsworth'
         alias.nick_aliases = []
@@ -83,6 +67,13 @@ class TestTell(unittest.TestCase):
         responses = {'I\'ll pass that on when tests is around.', 'yeah, yeah', 'yeah, sure, whatever'}
         out = self.phenny.reply.call_args[0][0]
         self.assertTrue(out in responses)
+
+    def test_fremind_edgecase(self):
+        self.input.nick = 'Testsworth'
+        self.input.groups = lambda: ['me', 'eat a cake']
+
+        tell.f_remind(self.phenny, self.input, 'ask')
+        self.phenny.say.assert_called_once_with('Hey, I\'m not as stupid as Monty you know!')
 
     def test_ftell(self):
         self.input.nick = 'Testsworth'
@@ -103,4 +94,11 @@ class TestTell(unittest.TestCase):
         responses = {'I\'ll pass that on when tests is around.', 'yeah, yeah', 'yeah, sure, whatever'}
         out = self.phenny.reply.call_args[0][0]
         self.assertTrue(out in responses)
-"""
+
+    def test_formatreminder(self):
+        dt = datetime.datetime.utcnow().strftime('%d %b %Y %H:%MZ')
+        ret = tell.formatReminder(['tests', 'ask', dt, 'to eat cake'], 'Testsworth', None)
+
+        dt = dt[len(datetime.datetime.utcnow().strftime('%d %b')) + 1:]
+        dt = dt.replace(datetime.datetime.utcnow().strftime('%Y '), '')
+        self.assertTrue(ret == 'Testsworth: %s <tests> ask Testsworth to eat cake' % dt)

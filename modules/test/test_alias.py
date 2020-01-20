17d538aa011a5ced711a95690eb0692f350fefcb
Maathavan <maathumaathavan@gmail.com>
2020-01-20 12:14:11 +0530

    Revert "Update"
    This reverts commit 17c42eb9176e778afa4eea603d1ce8a6241d346b.

--- modules/test/test_alias.py
+++ modules/test/test_alias.py
@@ -18,8 +18,8 @@ class TestAlias(unittest.TestCase):
 
     def create_alias(self, aliasName, input):
         self.input.group = lambda x: ['', 'add', aliasName][x]
-        alias.alias(self.phenny, input)
-        alias.aliasPairMerge(self.phenny, input.nick, aliasName)
+        aliasmod.alias(self.phenny, input)
+        aliasmod.aliasPairMerge(self.phenny, input.nick, aliasName)
 
     def create_reminder(self, teller):
         timenow = datetime.datetime.utcnow().strftime('%d %b %Y %H:%MZ')
@@ -27,7 +27,7 @@ class TestAlias(unittest.TestCase):
 
     def test_aliasGroupFor_multiple(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = []
+        aliasmod.nick_aliases = []
 
         aliases = ['tester', 'testing', 'testmaster']
 
@@ -36,20 +36,20 @@ class TestAlias(unittest.TestCase):
 
         aliases.insert(0,'Testsworth')
 
-        aligroup = alias.aliasGroupFor('Testsworth')
+        aligroup = aliasmod.aliasGroupFor('Testsworth')
         self.assertTrue(aliases == aligroup)
 
     def test_aliasGroupFor_singleton(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = []
+        aliasmod.nick_aliases = []
         aliases = ['Testsworth']
 
-        aligroup = alias.aliasGroupFor('Testsworth')
+        aligroup = aliasmod.aliasGroupFor('Testsworth')
         self.assertTrue(aliases == aligroup)
 
     def test_aliasPairMerge(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = []
+        aliasmod.nick_aliases = []
         aliases = ['tester', 'testing', 'testmaster']
         for aliasName in aliases:
             self.create_alias(aliasName, self.input)
@@ -59,100 +59,100 @@ class TestAlias(unittest.TestCase):
         for aliasName in aliases2:
             self.create_alias(aliasName, self.input2)
 
-        alias.aliasPairMerge(self.phenny, 'Testsworth', 'Happy')
+        aliasmod.aliasPairMerge(self.phenny, 'Testsworth', 'Happy')
 
         aliases.insert(0,'Testsworth')
         aliases2.insert(0,'Happy')
         joined = aliases + aliases2
 
-        self.assertTrue(joined in alias.nick_aliases)
+        self.assertTrue(joined in aliasmod.nick_aliases)
 
     def test_alias_noadded(self):
         self.input.nick = 'Testsworth'
         self.input.group = lambda x: ['alias', 'add', None][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Usage: .alias add <nick>')
 
     def test_alias_addself(self):
         self.input.nick = 'Testsworth'
         self.input.group = lambda x: ['alias', 'add', 'Testsworth'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('I don\'t think that will be necessary.')
 
     def test_alias_alreadypaired(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = [['Testsworth', 'tests']]
+        aliasmod.nick_aliases = [['Testsworth', 'tests']]
 
         self.input.group = lambda x: ['alias', 'add', 'tests'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('You and tests are already paired.')
 
     def test_alias_confirmreq(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = []
-        alias.nick_pairs.append(['tests', 'Testsworth'])
+        aliasmod.nick_aliases = []
+        aliasmod.nick_pairs.append(['tests', 'Testsworth'])
 
         self.input.group = lambda x: ['alias', 'add', 'tests'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Confirmed alias request with tests. Your current aliases are: Testsworth, tests.')
 
     def test_alias_alreadysent(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = []
-        alias.nick_pairs.append(['Testsworth', 'tests'])
+        aliasmod.nick_aliases = []
+        aliasmod.nick_pairs.append(['Testsworth', 'tests'])
 
         self.input.group = lambda x: ['alias', 'add', 'tests'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Alias request already exists. Switch your nick to tests and call \".alias add Testsworth\" to confirm.')
 
     def test_alias_valid(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = []
-        alias.nick_pairs = []
+        aliasmod.nick_aliases = []
+        aliasmod.nick_pairs = []
 
         self.input.group = lambda x: ['alias', 'add', 'tests'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Alias request created. Switch your nick to tests and call \".alias add Testsworth\" to confirm.')
 
     def test_alias_listothers(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases.append(['happy', 'joyous'])
+        aliasmod.nick_aliases.append(['happy', 'joyous'])
         self.input.group = lambda x: ['alias', 'list', 'happy'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('happy\'s current aliases are: happy, joyous.')
 
     def test_alias_listself(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases.append(['Testsworth', 'tests'])
+        aliasmod.nick_aliases.append(['Testsworth', 'tests'])
         self.input.group = lambda x: ['alias', 'list', None][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Your current aliases are: Testsworth, tests.')
 
     def test_alias_remove(self):
         self.input.nick = 'Testsworth'
-        alias.nick_aliases = [['Testsworth', 'tests']]
+        aliasmod.nick_aliases = [['Testsworth', 'tests']]
         self.input.group = lambda x: ['alias', 'remove'][x]
 
-        alias.alias(self.phenny, self.input)
-        self.assertTrue(alias.aliasGroupFor('Testsworth') == ['Testsworth'])
+        aliasmod.alias(self.phenny, self.input)
+        self.assertTrue(aliasmod.aliasGroupFor('Testsworth') == ['Testsworth'])
         self.phenny.reply.assert_called_once_with('You have removed Testsworth from its alias group')
 
     def test_alias_wrongcommand(self):
         self.input.group = lambda x: ['alias', 'eat'][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Usage: .alias add <nick>, .alias list <nick>?, .alias remove')
 
     def test_alias_noinput(self):
         self.input.group = lambda x: ['alias', None][x]
 
-        alias.alias(self.phenny, self.input)
+        aliasmod.alias(self.phenny, self.input)
         self.phenny.reply.assert_called_once_with('Usage: .alias add <nick>, .alias list <nick>?, .alias remove')

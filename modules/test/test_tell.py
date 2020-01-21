552552164d49c1b95fbe173072314c1423ebf0dd
Maathavan <maathumaathavan@gmail.com>
2020-01-21 10:58:09 +0530

    Removed unnecessary tests

--- modules/test/test_tell.py
+++ modules/test/test_tell.py
@@ -31,13 +31,6 @@ class TestTell(unittest.TestCase):
         text = ': You have messages. Say something, and I\'ll read them out.'
         self.phenny.say.assert_called_once_with(self.input.nick + text)
 
-    def test_fremind_toolong(self):
-        self.input.nick = 'Testsworth'
-        self.input.groups = lambda: ['testerrrrrrrrrrrrrrrrr', 'eat a cake']
-
-        tell.f_remind(self.phenny, self.input, 'ask')
-        self.phenny.reply.assert_called_once_with('That nickname is too long.')
-
     def test_fremind_valid(self):
         self.input.nick = 'Testsworth'
         self.input.groups = lambda: ['tests', 'eat a cake']
@@ -55,15 +48,6 @@ class TestTell(unittest.TestCase):
         self.phenny.say.assert_called_once_with('Hey, I\'m not as stupid as Monty you know!')
 
     def test_ftell(self):
-        self.input.nick = 'Testsworth'
-        self.input.groups = lambda: ['tests', 'eat a cake']
-
-        tell.f_tell(self.phenny, self.input)
-        responses = {'I\'ll pass that on when tests is around.', 'yeah, yeah', 'yeah, sure, whatever'}
-        out = self.phenny.reply.call_args[0][0]
-        self.assertTrue(out in responses)
-
-    def test_fask(self):
         self.input.nick = 'Testsworth'
         self.input.groups = lambda: ['tests', 'eat a cake']
 
